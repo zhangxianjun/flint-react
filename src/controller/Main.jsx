1b3117508bcd068b75766c81854daf403bf4ea0f
zhangxianjun <1581758003@qq.com>
2022-01-26 14:21:56 +0800

    feat -

---
+++ src/controller/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Main() {
+    // 声明一个新的叫做 “count” 的 state 变量
+    return (
+        <div>
+            <p>You clicked count times</p>
+        </div>
+    );
+}
