b8a4fd28941c93fb5aed5d0cdef8bdddfab4c020
zhangxianjun <1581758003@qq.com>
2022-01-27 15:25:59 +0800

    feat -

--- src/controller/Main.jsx
+++ src/controller/Main.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import {Container, Col, Row, Stack, Navbar, Nav} from 'react-bootstrap';
 
 export function Main() {
-    // 声明一个新的叫做 “count” 的 state 变量
     return (
-        <div>
-            <p>You clicked count times</p>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col lg={2}>
+
+                    <Navbar>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    </Navbar>
+
+                    <div className="bg-light border">
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            <Nav.Link href="/home">Active</Nav.Link>
+                            <Nav.Link eventKey="link-1">Link</Nav.Link>
+                            <Nav.Link eventKey="link-2">Link</Nav.Link>
+                            <Nav.Link eventKey="disabled" disabled>
+                                Disabled
+                            </Nav.Link>
+                        </Nav>
+                    </div>
+                </Col>
+                <Col lg={10}>
+                    <Navbar bg="light" expand="lg">
+                        <Container>
+                            <Navbar.Text>产品</Navbar.Text>
+                            <Navbar.Text>开发</Navbar.Text>
+                            <Navbar.Text>测试</Navbar.Text>
+                            <Navbar.Text>运维</Navbar.Text>
+                        </Container>
+                    </Navbar>
+
+                    <div className="bg-light border">Body</div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
