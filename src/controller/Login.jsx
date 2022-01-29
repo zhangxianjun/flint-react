82f38f504538d4bb4df06dfe7acf94f334b1d9c9
zhangxianjun <1581758003@qq.com>
2022-01-29 22:29:30 +0800

    feat - 调整样式

--- src/controller/Login.jsx
+++ src/controller/Login.jsx
@@ -1,4 +1,4 @@
-import {Button, Col, Container, Row, Form, Stack} from "react-bootstrap";
+import {Button, Col, Container, Row, Form, Stack, Card} from "react-bootstrap";
 
 export function Login(props) {
 
@@ -7,17 +7,18 @@ export function Login(props) {
     }
 
     return (
-        <Stack gap={2} className="col-lg-2 m-lg-auto">
-            <Form>
-                <Form.Group className="my-3" controlId="formBasicEmail">
-                    <Form.Control size="sm" type="email" placeholder="请输入账号"/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control size="sm" type="password" placeholder="请输入密码"/>
-                </Form.Group>
+        <Card className="col-xl-3 bg-light mx-xl-auto my-xl-5">
+            <Form className="col-xl-10 mx-xl-auto mt-xl-4">
+                <Form.Label className="mb-xl-3 ">Flint Admin</Form.Label>
+                <Form.Control className="mb-xl-3" size="sm" type="email" placeholder="请输入账号"/>
+                <Form.Control className="mb-xl-3" size="sm" type="password" placeholder="请输入密码"/>
             </Form>
-            <Button variant="primary" size="sm" onClick={login}>登录</Button>
-        </Stack>
+            <Button className="col-xl-10 mb-xl-4 mx-xl-auto" variant="primary" size="sm" onClick={login}>登录</Button>
+        </Card>
+
+        // <Stack gap={2} className="col-xl-2 m-xl-auto">
+        //
+        // </Stack>
 
     );
 }
