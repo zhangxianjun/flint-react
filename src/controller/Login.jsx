361a2f5366fb560098e87c400978514e1ec3cf02
zhangxianjun <1581758003@qq.com>
2022-01-30 16:56:35 +0800

    feat - 居中

--- src/controller/Login.jsx
+++ src/controller/Login.jsx
@@ -7,18 +7,19 @@ export function Login(props) {
     }
 
     return (
-        <Card className="col-xl-3 bg-light mx-xl-auto my-xl-5">
-            <Form className="col-xl-10 mx-xl-auto mt-xl-4">
-                <Form.Label className="mb-xl-3 ">Flint Admin</Form.Label>
-                <Form.Control className="mb-xl-3" size="sm" type="email" placeholder="请输入账号"/>
-                <Form.Control className="mb-xl-3" size="sm" type="password" placeholder="请输入密码"/>
-            </Form>
-            <Button className="col-xl-10 mb-xl-4 mx-xl-auto" variant="primary" size="sm" onClick={login}>登录</Button>
-        </Card>
-
-        // <Stack gap={2} className="col-xl-2 m-xl-auto">
-        //
-        // </Stack>
-
+        <Container className="vw-100 vh-100">
+            <Row className="h-100">
+                <Col className="align-self-center">
+                    <Card className="col-xl-3 bg-light mx-xl-auto">
+                        <Form className="col-xl-10 mx-xl-auto mt-xl-4">
+                            <Form.Label className="mb-xl-3 ">Flint Admin</Form.Label>
+                            <Form.Control className="mb-xl-3" size="sm" type="email" placeholder="请输入账号"/>
+                            <Form.Control className="mb-xl-3" size="sm" type="password" placeholder="请输入密码"/>
+                        </Form>
+                        <Button className="col-xl-10 mb-xl-4 mx-xl-auto" variant="primary" size="sm" onClick={login}>登录</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
